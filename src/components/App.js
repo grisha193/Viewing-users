9235af5b75d23c5285887e69e70eb52d4fc89b66
grisha193 <grisha.777111@mail.ru>
2025-03-20 23:56:56 +0300

    add btn

---
+++ src/components/App.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Header from "./Header";
+import Users from "./Users";
+import AddUser from "./AddUser";
+import axios from "axios";
+
+const baseUrl = "https://reqres.in/api/users?page=1"
+
+class App extends React.Component   
+{ 
+  constructor(props)
+   {
+      super(props)
+      axios.get(baseUrl).then((res) =>{
+        this.setState({users: res.data.data})
+      })
+
+
+      
+      this.state ={
+          users: []
+      }
+      this.addUser = this.addUser.bind(this)
+      this.deleteUser = this.deleteUser.bind(this)
+      this.editUser = this.editUser.bind(this)
+   }
+
+  render ()
+  {
+    return (<div> 
+    <Header title ="Список пользователей"/>
+    <main>
+       <Users users={this.state.users} onDelete={this.deleteUser} onEdit = {this.editUser}/>
+    </main>
+    <aside>
+      <AddUser onAdd={this.addUser} />
+    </aside>
+  </div>) 
+  }
+
+  deleteUser(id)
+  {
+    this.setState({
+      users: this.state.users.filter((el) => el.id !== id)
+    })
+  }
+
+  editUser(user)
+  {
+    let allUsers = this.state.users
+    allUsers[user.id - 1] = user
+
+    this.setState({users: []}, () => {
+        this.setState({users: [...allUsers] })
+    })
+  }
+
+
+
+  addUser(user)
+  {
+    const id = this.state.users.length + 1
+    this.setState({users: [...this.state.users, {id, ...user}]})
+  }
+
+  
+}
+ 
+
+
+export default App
